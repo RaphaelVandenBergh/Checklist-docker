07d39900abd0c0a4eeab513ae2858b38228e9123
RaphaelVandenBergh <r0793296@student.thomasmore.be>
2022-11-24 15:45:51 +0100

    test

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
+        {/* <ScrollRestoration /> */}
         <Scripts />
         <LiveReload />
       </body>
